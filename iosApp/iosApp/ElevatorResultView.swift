40188dfcd67f39ffbaf90ecbcc7c6a69ee09681f
whenyourapprun <whenyourapprun@gmail.com>
2022-11-11 17:35:54 +0900

    view 10개 이상인 경우 ViewBuilder 생성으로 해결

--- iosApp/iosApp/ElevatorResultView.swift
+++ iosApp/iosApp/ElevatorResultView.swift
@@ -30,7 +30,8 @@ struct ElevatorResultView: View {
                                     Row(title: "areaNm", detail: item.areaNm)
                                     Row(title: "buldMgtNo1", detail: item.buldMgtNo1)
                                     Row(title: "buldMgtNo2", detail: item.buldMgtNo2)
-                                    /*/Row(title: "buldNm", detail: item.buldNm)
+                                    Row(title: "buldNm", detail: item.buldNm)
+                                    //*/
                                     Row(title: "elevatorNo", detail: item.elevatorNo)
                                     Row(title: "elvtrAsignNo", detail: item.elvtrAsignNo)
                                     Row(title: "elvtrDetailForm", detail: item.elvtrDetailForm)
@@ -49,7 +50,7 @@ struct ElevatorResultView: View {
                                     Row(title: "shuttleSection", detail: item.shuttleSection)
                                     Row(title: "sigunguNm", detail: item.sigunguNm)
                                     Row(title: "undgrndFloorCnt", detail: item.undgrndFloorCnt)
-                                     // */
+                                    // */
                                 }
                             }
                         }
@@ -97,6 +98,33 @@ struct Row: View {
                 .font(.body)
                 .foregroundColor(.text)
         }
+    }
+}
+
+extension ViewBuilder {
+    /// `ViewBuilder` extension to build TupleView with 15 child view (using `Group` as well).
+    public static func buildBlock<C0, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, C13, C14, C15, C16, C17, C18, C19, C20, C21, C22, C23, C24, C25>
+    // Parameters.
+    (_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6, _ c7: C7, _ c8: C8, _ c9: C9, _ c10: C10, _ c11: C11, _ c12: C12, _ c13: C13, _ c14: C14, _ c15: C15, _ c16: C16, _ c17: C17, _ c18: C18, _ c19: C19, _ c20: C20, _ c21: C21, _ c22: C22, _ c23: C23, _ c24: C24, _ c25: C25)
+    // Return type.
+        -> TupleView<
+            (
+                Group<TupleView<(C0, C1, C2, C3, C4, C5, C6, C7, C8, C9)>>,
+                Group<TupleView<(C10, C11, C12, C13, C14, C15, C16, C17, C18, C19)>>,
+                Group<TupleView<(C20, C21, C22, C23, C24, C25)>>
+            )
+        >
+    // Type constraints.
+    where C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View, C7: View, C8: View, C9: View, C10: View, C11: View, C12: View, C13: View, C14: View, C15: View, C16: View, C17: View, C18: View, C19: View, C20: View, C21: View, C22: View, C23: View, C24: View, C25: View
+    // Function body.
+    {
+        TupleView(
+            (
+                Group { TupleView((c0, c1, c2, c3, c4, c5, c6, c7, c8, c9)) },
+                Group { TupleView((c10, c11, c12, c13, c14, c15, c16, c17, c18, c19)) },
+                Group { TupleView((c20, c21, c22, c23, c24, c25)) }
+            )
+        )
     }
 }
 
