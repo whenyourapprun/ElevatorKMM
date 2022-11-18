ecd88fcd41c4ffffd1fe4c56bbd0881b3ba6d0ca
whenyourapprun <whenyourapprun@gmail.com>
2022-11-19 03:24:17 +0900

    인트로 화면에 테마 적용

--- androidApp/src/main/java/com/whenyourapprun/elevator/android/IntroActivity.kt
+++ androidApp/src/main/java/com/whenyourapprun/elevator/android/IntroActivity.kt
@@ -1,78 +1,75 @@
 package com.whenyourapprun.elevator.android
 
 import android.content.Intent
-import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.util.Timer
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
+import com.whenyourapprun.elevator.android.ui.theme.ElevatorTheme
 import kotlin.concurrent.timer
 
 class IntroActivity : ComponentActivity() {
+    companion object {
+        private const val TAG = "IntroActivity"
+    }
     // 타이머에서 사용할 변수
     private var seconds by mutableStateOf(0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scaffoldState = rememberScaffoldState()
-            val coroutineScope = rememberCoroutineScope()
-            // 발판 사용 - 머터리얼
-            Scaffold {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(it)
-                        .background(colorResource(id = R.color.back)),
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Box(
+            ElevatorTheme {
+                val scaffoldState = rememberScaffoldState()
+                val coroutineScope = rememberCoroutineScope()
+                // 발판 사용 - 머터리얼
+                Scaffold {
+                    Column(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp, 0.dp)
-                            .background(Color.White)
+                            .fillMaxSize()
+                            .padding(it)
+                            .background(colorResource(id = R.color.back)),
+                        verticalArrangement = Arrangement.Center
                     ) {
-                        Column() {
-                            Spacer(modifier = Modifier.height(16.dp))
-                            Text(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(16.dp, 8.dp)
-                                    .background(Color.White),
-                                textAlign = TextAlign.Center,
-                                fontSize = 21.sp,
-                                color = colorResource(id = R.color.text),
-                                text = stringResource(R.string.Elevator)
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                            LinearProgressIndicator(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .height(16.dp)
-                                    .padding(16.dp, 0.dp),
-                                backgroundColor = colorResource(id = R.color.not_selected),
-                                color = colorResource(id = R.color.selected),
-                                progress = seconds.toFloat() / 100.0f
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp, 0.dp)
+                                .background(Color.White)
+                        ) {
+                            Column() {
+                                Spacer(modifier = Modifier.height(16.dp))
+                                Text(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(16.dp, 8.dp)
+                                        .background(Color.White),
+                                    textAlign = TextAlign.Center,
+                                    fontSize = 21.sp,
+                                    color = colorResource(id = R.color.text),
+                                    text = stringResource(R.string.Elevator)
+                                )
+                                Spacer(modifier = Modifier.height(16.dp))
+                                LinearProgressIndicator(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(16.dp)
+                                        .padding(16.dp, 0.dp),
+                                    backgroundColor = colorResource(id = R.color.not_selected),
+                                    color = colorResource(id = R.color.selected),
+                                    progress = seconds.toFloat() / 100.0f
+                                )
+                                Spacer(modifier = Modifier.height(16.dp))
+                            }
                         }
                     }
                 }
